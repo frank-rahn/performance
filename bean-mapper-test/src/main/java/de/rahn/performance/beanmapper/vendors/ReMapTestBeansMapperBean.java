b986b8f08b6a8430dfd9fc212259f7b9921fdd15
Tom Hombergs <tom.hombergs@gmail.com>
2017-10-04 00:02:58 +0200

    Instanziierung der Mapper im Konstruktor anstatt in der map() Methode

--- bean-mapper-test/src/main/java/de/rahn/performance/beanmapper/vendors/ReMapTestBeansMapperBean.java
+++ bean-mapper-test/src/main/java/de/rahn/performance/beanmapper/vendors/ReMapTestBeansMapperBean.java
@@ -15,6 +15,38 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReMapTestBeansMapperBean implements TestBeansMapperBean {
 
+  private Mapper<XmlRow, DomainRow> xmlRowToDomainRowMapper;
+
+  private Mapper<XmlTable, DomainTable> xmlTableToDomainTableMapper;
+
+  private Mapper<DomainRow, XmlRow> domainRowToXmlRowMapper;
+
+  private Mapper<DomainTable, XmlTable> domainTableToXmlTableMapper;
+
+  public ReMapTestBeansMapperBean() {
+    this.xmlRowToDomainRowMapper = Mapping
+        .from(XmlRow.class)
+        .to(DomainRow.class)
+        .mapper();
+    this.xmlTableToDomainTableMapper = Mapping
+        .from(XmlTable.class)
+        .to(DomainTable.class)
+        .replace(XmlTable::getDate, DomainTable::getDate)
+        .with(calendarToDate())
+        .useMapper(xmlRowToDomainRowMapper)
+        .mapper();
+    this.domainRowToXmlRowMapper = Mapping
+        .from(DomainRow.class)
+        .to(XmlRow.class)
+        .mapper();
+    this.domainTableToXmlTableMapper = Mapping.from(DomainTable.class)
+        .to(XmlTable.class)
+        .replace(DomainTable::getDate, XmlTable::getDate)
+        .with(dateToCalendar())
+        .useMapper(this.domainRowToXmlRowMapper)
+        .mapper();
+  }
+
   @Override
   public String getMapperName() {
     return "reMap";
@@ -22,41 +54,17 @@ public class ReMapTestBeansMapperBean implements TestBeansMapperBean {
 
   @Override
   public XmlTable map(DomainTable source) throws Exception {
-    Mapper<DomainRow, XmlRow> rowMapper =
-        Mapping.from(DomainRow.class)
-            .to(XmlRow.class)
-            .mapper();
-
-    Mapper<DomainTable, XmlTable> tableMapper = Mapping.from(DomainTable.class)
-        .to(XmlTable.class)
-        .replace(DomainTable::getDate, XmlTable::getDate)
-        .with(dateToCalendar())
-        .useMapper(rowMapper)
-        .mapper();
-
-    return tableMapper.map(source);
+    return domainTableToXmlTableMapper.map(source);
   }
 
   @Override
   public DomainTable map(XmlTable source) throws Exception {
-    Mapper<XmlRow, DomainRow> rowMapper =
-        Mapping.from(XmlRow.class)
-            .to(DomainRow.class)
-            .mapper();
-
-    Mapper<XmlTable, DomainTable> tableMapper = Mapping.from(XmlTable.class)
-        .to(DomainTable.class)
-        .replace(XmlTable::getDate, DomainTable::getDate)
-        .with(calendarToDate())
-        .useMapper(rowMapper)
-        .mapper();
-
-    return tableMapper.map(source);
+    return xmlTableToDomainTableMapper.map(source);
   }
 
   private Transform<Calendar, Date> dateToCalendar() {
     return source -> {
-      if(source == null){
+      if (source == null) {
         return null;
       }
       Calendar c = Calendar.getInstance();
@@ -67,7 +75,7 @@ public class ReMapTestBeansMapperBean implements TestBeansMapperBean {
 
   private Transform<Date, Calendar> calendarToDate() {
     return source -> {
-      if(source == null){
+      if (source == null) {
         return null;
       }
       return source.getTime();
